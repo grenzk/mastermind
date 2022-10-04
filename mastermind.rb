d2cd49c8d00327f89764824993247d194a001272
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-04 13:32:43 +0800

    Change display row output

--- mastermind.rb
+++ mastermind.rb
@@ -78,8 +78,6 @@ class Mastermind
 
   def display_row
     puts "| #{slots[0]} | #{slots[1]} | #{slots[2]} | #{slots[3]} |"
-    puts '-------------'
-    puts "| #{smaller_slots[0]} | #{smaller_slots[1]} | #{smaller_slots[2]} | #{smaller_slots[3]} |"
   end
 
   def play
